1ff2c704981d182b926afece8269aac77eabb655
WhoIsCamilo24 <cvillalobos@edtools.co>
2026-02-24 10:27:06 -0500

    colocando formato de creacion de correo cucusa.org

--- edtools_core/overrides/student.py
+++ edtools_core/overrides/student.py
@@ -11,10 +11,28 @@ except ImportError:
     from education.education.doctype.student.student import Student as EducationStudent
 
 import frappe
+from frappe import _
+
+from edtools_core.azure_provisioning import generate_cucusa_email
 
 
 class Student(EducationStudent):
     def validate_user(self):
+        # Asegurar email desde Applicant si falta (evita AttributeError en User.autoname)
+        if self.student_applicant and not self.student_email_id:
+            email = frappe.db.get_value(
+                "Student Applicant", self.student_applicant, "student_email_id"
+            )
+            if email:
+                self.student_email_id = email
+            else:
+                # Generar correo institucional primernombre.segundonombre.apellido1.apellido2@cucusa.org
+                applicant = frappe.get_doc("Student Applicant", self.student_applicant)
+                self.student_email_id = generate_cucusa_email(
+                    applicant.first_name,
+                    applicant.middle_name,
+                    applicant.last_name,
+                )
         # Durante Azure provisioning, el User @cucusa.org ya fue creado en enrollment.py.
         # Evitar DuplicateEntryError si validate_user intenta crearlo (exists puede fallar).
         if getattr(frappe.flags, "azure_provisioning_enroll", False) and self.student_email_id and self.student_email_id.endswith("@cucusa.org"):
