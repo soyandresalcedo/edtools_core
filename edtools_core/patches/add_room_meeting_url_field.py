a8c88289e30ee526fec0d84294dda43fa815b004
WhoIsCamilo24 <cvillalobos@edtools.co>
2026-03-17 17:36:00 -0500

    Arreglando aviso de 140 caracteres room

--- edtools_core/patches/add_room_meeting_url_field.py
+++ edtools_core/patches/add_room_meeting_url_field.py
@@ -1,5 +1,8 @@
 # Copyright (c) Edtools
 # Agrega el campo meeting_url al DocType Room para enlaces de reunión virtual (Teams, Zoom, etc.).
+# Límite 500 caracteres para soportar URLs largas de Teams, Zoom, etc.
+
+MEETING_URL_LENGTH = 500
 
 import frappe
 
@@ -16,11 +19,23 @@ def execute():
 			"fieldname": "meeting_url",
 			"label": "Meeting URL",
 			"fieldtype": "Data",
+			"length": MEETING_URL_LENGTH,
 			"description": "URL de Teams, Zoom o otra plataforma para clases virtuales. Se muestra como enlace en el horario del portal.",
 			"insert_after": "room_number",
 		})
 		cf.insert(ignore_permissions=True)
 		frappe.db.commit()
-		frappe.logger().info("Custom Field Room.meeting_url created")
+		frappe.logger().info("Custom Field Room.meeting_url created (length=%s)", MEETING_URL_LENGTH)
 	else:
-		frappe.logger().info("Custom Field Room.meeting_url already exists")
+		# Actualizar longitud si ya existía con el valor por defecto (140)
+		cf = frappe.get_doc("Custom Field", custom_field_name)
+		current_length = int(cf.get("length") or 0)
+		if current_length < MEETING_URL_LENGTH:
+			cf.length = MEETING_URL_LENGTH
+			cf.save(ignore_permissions=True)
+			frappe.db.commit()
+			# Cambiar tipo de columna en la base de datos (PostgreSQL: varchar(500))
+			frappe.db.change_column_type("Room", "meeting_url", "varchar(%s)" % MEETING_URL_LENGTH)
+			frappe.logger().info("Custom Field Room.meeting_url length updated to %s", MEETING_URL_LENGTH)
+		else:
+			frappe.logger().info("Custom Field Room.meeting_url already exists")
