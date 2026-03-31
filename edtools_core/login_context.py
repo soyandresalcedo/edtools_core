807cf65ec7a106b2de31315f38c42dcae28e872d
WhoIsCamilo24 <cvillalobos@edtools.co>
2026-03-31 15:18:02 -0500

    Arreglo imagenes login #2

--- edtools_core/login_context.py
+++ edtools_core/login_context.py
@@ -11,7 +11,7 @@ def patch_login_context():
 	def get_context(context):
 		original_get_context(context)
 		# Forzar logo y nombre en la página de login
-		context["logo"] = "/assets/edtools_core/images/cuc-university-logo.png"
+		context["logo"] = "/assets/edtools_core/images/iditek-logo.png?v=20260331"
 		context["app_name"] = "IDITEK"
 
 	login_module.get_context = get_context
