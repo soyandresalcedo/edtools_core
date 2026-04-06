9323383d5679a5b4170c811c4a17b8863102ae3d
WhoIsCamilo24 <cvillalobos@edtools.co>
2026-04-06 15:07:36 -0500

    Arreglando deploy nueva herramienta

---
+++ edtools_core/edtools_core/doctype/student_financial_plan_student/student_financial_plan_student.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2026, EdTools and contributors
+# For license information, please see license.txt
+
+from frappe.model.document import Document
+
+
+class StudentFinancialPlanStudent(Document):
+	pass
